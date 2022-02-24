1f04737dd515f0a93b38a5b14c89d9f1dd07aa72
yukinissie <bigtree2490@outlook.jp>
2022-02-24 10:31:41 +0900

    init

---
+++ src/main/java/com/yukinissie/cqrssample/CqrsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.yukinissie.cqrssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yukinissie/cqrssample/CqrsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukinissie.cqrssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
